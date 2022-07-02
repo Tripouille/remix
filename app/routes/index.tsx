3abc9c89a5f41c5ed48c57686e1e3d903c266b35
Jean-Michel <jmichel.gambard@gmail.com>
2022-07-02 16:18:21 +0200

    feat: update routes index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,24 @@
 /** @format */
 
+import { Link } from "@remix-run/react";
+
+import type { LinksFunction } from "@remix-run/node";
+
+import styles from "../styles/index.css";
+
+export const links: LinksFunction = () => {
+    return [
+        {
+            rel: "stylesheet",
+            href: styles,
+        },
+    ];
+};
+
 export default function Index() {
-    return <p>Hello Index Route</p>;
+    return (
+        <div>
+            <Link to="/jokes">Visit jokes</Link>
+        </div>
+    );
 }
