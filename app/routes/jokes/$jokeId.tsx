f19291c8ad8f88f35fdf1f0f727638c059efef22
Jean-Michel <jmichel.gambard@gmail.com>
2022-07-02 18:41:44 +0200

    feat: add some jokes

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,5 +1,25 @@
 /** @format */
 
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+import type { Joke } from "@prisma/client";
+
+type LoaderData = {
+    joke: Joke;
+};
+
+/** @format */
+export const loader: LoaderFunction = async ({ params }) => {
+    const { jokeId } = params;
+    const joke = await db.joke.findUnique({
+        where: { id: jokeId },
+    });
+    const data = { joke };
+    return json(data);
+};
+
 export default function JokeId() {
-    return <p>ID</p>;
+    const data = useLoaderData<LoaderData>();
+    return <p>{data.joke.content}</p>;
 }
