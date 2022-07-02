af3070a1f985933841d1742aa4e491f8f5d9413f
Jean-Michel <jmichel.gambard@gmail.com>
2022-07-02 17:26:57 +0200

    feat(jokes): update contents

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,24 +1,29 @@
 /** @format */
 
+import type { LinksFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 
-import type { LinksFunction } from "@remix-run/node";
-
-import styles from "../styles/index.css";
+import stylesUrl from "~/styles/index.css";
 
 export const links: LinksFunction = () => {
-    return [
-        {
-            rel: "stylesheet",
-            href: styles,
-        },
-    ];
+    return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export default function Index() {
+export default function IndexRoute() {
     return (
-        <div>
-            <Link to="/jokes">Visit jokes</Link>
+        <div className="container">
+            <div className="content">
+                <h1>
+                    Remix <span>Jokes!</span>
+                </h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="jokes">Read Jokes</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 }
